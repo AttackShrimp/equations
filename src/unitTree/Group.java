317573ea1c35356281f1aaa7dff3fa53898aacd4
AttackShrimp <danielsantamarinapuertas@gmail.com>
2021-01-22 15:47:42 +0100

    Group equation creation and polynomial creation methods structure

--- src/unitTree/Group.java
+++ src/unitTree/Group.java
@@ -6,4 +6,22 @@ public class Group extends Operable {
     List<Operable> operables;
     List<Link> links;
     Polinomial polinomial;
+
+    public void parseEquation(String equation) {
+        Link currLink = new Link();
+        OperablePair currPair = currLink.parsePair(equation);
+        while (currPair != null) {
+            operables.addAll(currPair.getPair());
+            links.add(currLink);
+            currLink = new Link();
+            currPair = currLink.parsePair(equation);
+        }
+    }
+
+    public void generatePolinomial() {
+        operables.stream().filter(op -> op instanceof Group).forEach((Group g) -> {
+            g.generatePolinomial();
+            polinomial.add(g.polinomial);
+        });
+    }
 }
