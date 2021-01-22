198d8766d1631db5d368b3a88a0c5be073d1f9cb
AttackShrimp <danielsantamarinapuertas@gmail.com>
2021-01-22 16:34:52 +0100

    Implement Link.parsePair double pattern matching

--- src/unitTree/Link.java
+++ src/unitTree/Link.java
@@ -1,11 +1,20 @@
 package unitTree;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Link {
-    Operable firstOperable;
-    Operable secondOperable;
+    OperablePair operablePair;
     Operation operation;
 
     public OperablePair parsePair(StringBuilder equation) {
+        Pattern pattern = Pattern.compile("[-+]?[0-9]*\\.?[0-9]+([eE][-+]?[0-9]+)?");
+        Matcher matcher = pattern.matcher(equation);
+        matcher.find();
+        Operable firstOperable = new Unit(Double.parseDouble(equation.substring(0, matcher.start())));
+
+        equation.delete(0, matcher.start());
+
         return null;
     }
 }
