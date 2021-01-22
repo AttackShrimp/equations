1b759ab920d1e196e0c06a65ebb64e65ed41a1e2
AttackShrimp <danielsantamarinapuertas@gmail.com>
2021-01-22 16:04:53 +0100

    Implement Polynomial.add

--- src/unitTree/Polynomial.java
+++ src/unitTree/Polynomial.java
@@ -1,11 +1,18 @@
 package unitTree;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Polynomial {
+    int maxDegree;
     List<List<Unit>> units;
 
     public void add(Polynomial polynomial) {
-
+        for (int i = 0; i < maxDegree; i++) {
+            units.get(i).addAll(polynomial.units.get(i));
+        }
+        for (int i = maxDegree; i < polynomial.maxDegree; i++) {
+            units.add(new ArrayList<>(polynomial.units.get(i)));
+        }
     }
 }
