566cc82c4dee85c0d3371eec490c505fa54cea24
AttackShrimp <danielsantamarinapuertas@gmail.com>
2021-01-27 20:17:54 +0100

    Revert "Refactor Unit to move Polynomial functionality to PolynomialUnit"
    This reverts commit 5829e39c

--- src/unitTree/Unit.java
+++ src/unitTree/Unit.java
@@ -3,16 +3,19 @@ package unitTree;
 public class Unit extends Operable {
     public static final Unit EMPTY_UNIT = new Unit(0);
 
+    int polynomialDegree;
     double constant;
     char id;
 
     public Unit(double constant) {
+        polynomialDegree = 0;
         this.constant = constant;
     }
 
     public Unit(double constant, char id) {
         this.constant = constant;
         this.id = id;
+        polynomialDegree = 1;
     }
 
     public Unit(char id) {
@@ -24,6 +27,7 @@ public class Unit extends Operable {
         if (obj instanceof Unit) {
             Unit cmp = (Unit) obj;
             return cmp.constant == this.constant &&
+                    cmp.polynomialDegree == this.polynomialDegree &&
                     cmp.id == this.id;
         }
         return false;
@@ -32,8 +36,10 @@ public class Unit extends Operable {
     @Override
     public String toString() {
         String res = "";
-        res += (constant == 1 || constant == 0) ? "" : constant;
-        res += (constant == 0) ? "" : id;
+        res += (constant == 1) ? "" : constant;
+        res += (constant == 0 || polynomialDegree == 0) ? "" : id;
+        res += (polynomialDegree == 0 && constant == 1) ? 1 : "";
+        res += (constant == 0 || polynomialDegree == 1) ? "" : "^" + polynomialDegree;
         return res;
     }
 }
