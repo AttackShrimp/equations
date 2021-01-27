b946702de0c02d514afc9ba27e147cffcadfc18b
AttackShrimp <danielsantamarinapuertas@gmail.com>
2021-01-27 23:35:32 +0100

    Remove outdated code from Polynomial class

--- src/unitTree/Polynomial.java
+++ src/unitTree/Polynomial.java
@@ -1,29 +1,11 @@
 package unitTree;
 
-import java.util.ArrayList;
 import java.util.List;
 
-public class Polynomial {
+public class Polynomial extends Operable {
     List<PolynomialLevel> levels;
 
-    public Polynomial(List<Operable> operables, List<Link> links, List<Polynomial> innerPolynomials) {
-        levels = new ArrayList<>();
-
-        innerPolynomials.forEach(p -> {
-            int i = 0;
-            while (i < p.levels.size() && i < levels.size()) {
-                levels.get(i).add(p.levels.get(i));
-                i++;
-            }
-            while (i < p.levels.size()) {
-                levels.add(p.levels.get(i).copy());
-                i++;
-            }
-        });
-
-        operables.stream().filter(op -> op instanceof Unit).forEach(op -> {
-            Unit unit = (Unit) op;
-            
-        });
+    public Polynomial(List<Link> links) {
+        // links should only have one value, represent as a linked list where a vertex holds an operation
     }
 }
